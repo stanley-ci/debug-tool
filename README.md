944a0dca70d1e499e2030807a964ad95c4f77c2c
stanley-ci <huang.shaoliang@citcon.cn>
2020-09-09 16:41:24 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # debug-tool
+显示所有PAYPAL的key: node index show
+清除PAYPAL的token: node index clear
---
+++ index.js
@@ -0,0 +1,97 @@
+
+const redis = require('ioredis');
+require('dotenv').config();
+const { REDIS_HOST, REDIS_PORT, getRedisIndustrCodyKey } = require('../config');
+
+const redisCli = new redis.Cluster(
+    [
+        {
+            host: REDIS_HOST,
+            port: REDIS_PORT,
+        },
+    ],
+    {
+        dnsLookup: (address, callback) => callback(null, address),
+        redisOptions: {
+            tls: !process.env.REDIS_TLS ? null : {},
+            password: process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : null,
+        },
+    }
+);
+
+redisCli.on("error", function (error) {
+    console.error({ title: 'redis error', error: error.stack });
+});
+
+redisCli.on('connect', function () {
+    redisCli.connected = false;
+    const tId = setTimeout(() => {
+        console.error({ title: 'redis connecting failed!' });
+    }, 1000);
+
+    redisCli.ping(err => {
+        clearTimeout(tId);
+        if (err) {
+            console.error({ title: 'redis connecting error!' });
+            return;
+        }
+        redisCli.connected = true;
+        console.info({ title: 'redis connected successfully!' });
+        command();
+    });
+});
+
+
+
+function command() {
+    const cmd = process.argv[2];
+    console.log("您输入的命令:", cmd);
+    if (cmd === 'show') {
+        keys();
+    } else if (cmd === 'clear') {
+        clearRedis();
+    }
+
+}
+
+function keys() {
+    if (!redisCli.connected) {
+        console.error('redis disconnectied!');
+        return false;
+    }
+    redisCli.keys('PAY*', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('keys PAY*', JSON.stringify(data, null, 4));
+    });
+
+
+    redisCli.lrange(REDIS_TOKEN_KEY, 0, -1, (err, data) => {
+        if (err || data.length < 3) {
+            console.error({ title: 'redis token get error:', err, data });
+            return;
+        }
+
+        const [expires_in, created, desc] = data;
+        const isExpired = Date.now() / 1000 - created > expires_in - 60;
+        console.log({ title: 'redis token', isExpired, expires_in, created, desc })
+    });
+
+}
+
+function clearRedis() {
+    if (!redisCli.connected) {
+        console.error('redis disconnectied!');
+        return false;
+    }
+    redisCli.keys('PAY*', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log('keys PAY*');
+    });
+}
